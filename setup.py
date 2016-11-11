f599969c863a6e65fa42f75776d9bd0e127be47d
Robert Brennan <robert.brnnn@gmail.com>
2016-11-11 23:01:42 +0000

    Added compatability fix for pip 9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements('requirements.txt')
+install_reqs = parse_requirements('requirements.txt', session=False)
 reqs = [str(ir.req) for ir in install_reqs]
 
 
